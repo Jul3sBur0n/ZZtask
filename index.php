8b882224e8957b87b68bb1cf7f6481277dca22b9
Jules BURON <juburon1@etud.rcisima.isima.fr>
2016-01-14 17:22:38 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DCOTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="back.css" >
+		<title>
+			Le site de Zeni
+		</title>
+	</title>
+	<header>
+	</header>
+	<body>
+		<div class = "index">
+			<h1><p><FONT color=#ff000> Index </FONT></p></h1>
+			<nav>
+				<dl>
+					<dt><h3><a href="connexion.php" title="projet de gestion de tâche.
+(Test de commande html, css, php et javascript pour le moment"> Projet web</a></h3></li>
+				</dl>
+			</nav>
+		</div>
+	</body>
+</html>
