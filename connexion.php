498d1353c6717300f7c987351e128b2c7f29373f
Antoine JOULIA <anjoulia@etud.rcisima.isima.fr>
2016-01-14 17:53:19 +0100

    Joulia commit

---
+++ connexion.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<?php
+if(!isset($_SESSION))
+{
+session_start();
+}
+?>
+
+<link rel="stylesheet" href="back.css">
+
+<html>
+	<head>
+		<meta charset="utf_8"/>
+		<title>
+			Connexion
+		</title>
+	
+	</head>
+	<body>
+		<div class="connexion">
+		<form class="premier" name ="Connexion" method="post" action="verif_connexion.php">
+		<input class="input-form" type="text" name="pseudo" id="pseudo" placeholder="Login" title="4 à 15 caractères"/><br/>
+		<input class="input-form" type="text" name="password" id="password" placeholder="Password" title="4 à 20 caractères"/> <br/><br/>
+		<button class="btn" type="submit">Connexion<br/></button> <span class="reg"><?php echo'<a href="registeration.php" > Pas encore inscrit ? </a>';  ?></span>
+		</form>
+		</div>
+	</body>
+</html>
