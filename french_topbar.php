9424aad206b3083698ff66e29e2aa16ef6af6da4
Jules BURON <juburon1@etud.rcisima.isima.fr>
2016-01-21 17:18:23 +0100

    pages de langues et de roles

---
+++ french_topbar.php
@@ -0,0 +1,7 @@
+<?php
+$TXT_ROLE_0 = "Utilisateur";
+$TXT_ROLE_1 = "Administrateur";
+$TXT_YOU = "Vous êtes";
+$TXT_SIGNOUT = "Déconnexion";
+$TXT_CREATE = "Gestion des comptes";
+?>
--- tasklist.php
+++ tasklist.php
@@ -4,31 +4,42 @@
 	<head>
 		<meta charset="UTF-8">
 		<title> Liste des taches </title>
-		<link rel="stylesheet" type="text/css" href="task.css">
+		<link rel="stylesheet" type="text/css" href="default.css">
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
 	
 	</head>
 	
-	<body>
-		<div id="topbar">
-			<h2>Menu</h2>
-			<form>
-				<input type="button" name="NewTask" value="Add Task" onclick="self.location.href='newtask.php'" style="background-color:#3cb371" style="color:white; font-weight:bold"onclick> 
-			</form>
+	<body class ="fullpage logged-out">
+		
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-8 topbar"><h2>Menu</h2></div>
+				<div class="col-xs-6 col-md-4"><a class="btn btn-default" onclick="self.location.href='newtask.php'" role="button">Link</a></div>
+			</div>
 		</div>
 		
-		<div id="todo">
-			<h2>To Do</h2>
-		
-		</div>
-		
-		<div id="inprogress">
-			<h2>In Progress</h2>
-		
-		</div>
-		
-		<div id="done">
-			<h2>Done</h2>
 			
+		<div id="topbar"></div>
+		
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-6 col-md-4 todo">
+						
+							<h2>To Do</h2>
+						
+				</div>
+				<div class="col-xs-6 col-md-4">
+						<div class="inprogress">
+							<h2>In Progress</h2>
+						</div>
+				</div>
+				<div class="col-xs-6 col-md-4">
+						<div class="done">
+							<h2>Done</h2>
+						</div>
+				</div>
+			</div>
 		</div>
 	
 	</body>
