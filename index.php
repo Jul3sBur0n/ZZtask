6335bc501640876fe977c12afcb559e80ea39726
anjoulia <antoine.joulia@live.fr>
2016-01-21 18:27:51 +0100

    gri

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ include 'sessionstarter.php';
 	<body>
 		<div class="connexion">
 		<form class="premier" name ="Connexion" method="post" action="verif_connexion.php">
-		<input class="input-form" type="text" name="pseudo" id="pseudo" placeholder="Login" title="4 à 15 caractères" "<?php if(isset($_SESSION)) {echo 'value = "$_SESSION[\'login\']"';} ?>" /><br/>
-		<input class="input-form" type="password" name="password" id="password" placeholder="Password" title="4 à 20 caractères"/> <br/><br/>
+		<input class="input-form" type="text" name="pseudo" id="pseudo" placeholder="Login" title="4 à 15 caractères" "<?php if(isset($_SESSION)) {echo 'value = "$_SESSION[\'login\']"';} ?>" required /><br/>
+		<input class="input-form" type="password" name="password" id="password" placeholder="Password" title="4 à 20 caractères" required /> <br/><br/>
 		<button class="btn" type="submit">Connexion<br/></button> <span class="reg"><?php echo'<a href="registeration.php" > Pas encore inscrit ? </a>';  ?></span>
 		</form>
 		<?php
