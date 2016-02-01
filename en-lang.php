7257dd386444fce4ce11a0546b0dbf0176d7e579
Jules BURON <juburon1@etud.rcisima.isima.fr>
2016-02-01 13:15:58 +0100

    dico

---
+++ en-lang.php
@@ -0,0 +1,31 @@
+<?php
+
+define('TXT_LANG', 'FR');
+
+//------------------------------------------------------
+// index.php & registration.php
+//------------------------------------------------------
+
+define('TXT_CONNEXION', 'Sign In');
+define('TXT_ENREGISTREMENT', 'Sign Up');
+
+//------------------------------------------------------
+// errors
+//------------------------------------------------------
+
+define('TXT_ERROR_10', 'Error empty field');
+define('TXT_ERROR_20', 'Error during identification');
+define('TXT_ERROR_30', 'Login already used');
+define('TXT_ERROR_40', 'Characters not allowed (! ; :)');
+define('TXT_ERROR_50', 'Insufficient number of characters (min 4)');
+define('TXT_ERROR_51', 'Insufficient number of characters (min 5)');
+
+//------------------------------------------------------
+// function_task_user.php
+//------------------------------------------------------
+
+define('TXT_TACHE', 'Task');
+define('TXT_FIN', 'End');
+define('TXT_DESCRIPTION', 'Description');
+
+?>
---
+++ fr-lang.php
@@ -0,0 +1,31 @@
+<?php
+
+define('TXT_LANG', 'FR');
+
+//------------------------------------------------------
+// index.php & registration.php
+//------------------------------------------------------
+
+define('TXT_CONNEXION', 'Connexion');
+define('TXT_ENREGISTREMENT', 'Inscription');
+
+//------------------------------------------------------
+// errors
+//------------------------------------------------------
+
+define('TXT_ERROR_10', 'Erreur champ vide');
+define('TXT_ERROR_20', 'Erreur d\'identification');
+define('TXT_ERROR_30', 'Nom d\'utilisateur déjà utilisé');
+define('TXT_ERROR_40', 'Caractère non admis (! ; :)');
+define('TXT_ERROR_50', 'Nombre de caractère insuffisant (min 4)');
+define('TXT_ERROR_51', 'Nombre de caractère insuffisant (min 5)');
+
+//------------------------------------------------------
+// function_task_user.php & function_task_admin.php
+//------------------------------------------------------
+
+define('TXT_TACHE', 'Tâche');
+define('TXT_FIN', 'Fin');
+define('TXT_DESCRIPTION', 'Description');
+
+?>
