cf045d5bba7c757c9c8c20b235cfd1b352726d11
anjoulia <antoine.joulia@live.fr>
2016-02-03 20:26:15 +0100

    test avec espace dans le nom des tache

--- test_task.php
+++ test_task.php
@@ -6,14 +6,14 @@ class TestTasks extends PHPUnit_Framework_TestCase{
 	
 	public function testAddTask(){
 		newtask("Test","23-01-2016","This is a test");
-		newtask("test 2","23/25/2016","");
+		newtask("tache et plus","23/25/2016","");
 		
 		$fileCont = file_get_contents( "db_task.txt" );
 		$lines = explode( "\n", $fileCont );
 		$line = $lines[count($lines)-3];
 		$this->assertEquals("1;:!:;Test;:!:;23-01-2016;:!:;This is a test",$line);
 		$line = $lines[count($lines)-2];
-		$this->assertEquals("1;:!:;test 2;:!:;23/25/2016",$line);
+		$this->assertEquals("1;:!:;tache et plus;:!:;23/25/2016",$line);
 	
 		
 	}
@@ -24,8 +24,8 @@ class TestTasks extends PHPUnit_Framework_TestCase{
 		
 		$lignes = searchtask("Test3");
 		$this->assertEquals("1;:!:;Test3;:!:;23/25/2016\n",$lignes);
-		$lignes = searchtask("test 2");
-		$this->assertEquals("1;:!:;test 2;:!:;23/25/2016\n",$lignes);
+		$lignes = searchtask("tache et plus");
+		$this->assertEquals("1;:!:;tache et plus;:!:;23/25/2016\n",$lignes);
 		$lignes = searchtask("Test");
 		$this->assertEquals("1;:!:;Test;:!:;23-01-2016;:!:;This is a test\n",$lignes);
 	}	
@@ -35,7 +35,7 @@ class TestTasks extends PHPUnit_Framework_TestCase{
 		$ligne = gettask();
 
 		$this->assertEquals("1;:!:;Test;:!:;23-01-2016;:!:;This is a test\n",$ligne[0]);
-		$this->assertEquals("1;:!:;test 2;:!:;23/25/2016\n",$ligne[1]);
+		$this->assertEquals("1;:!:;tache et plus;:!:;23/25/2016\n",$ligne[1]);
 		$this->assertEquals("1;:!:;Test3;:!:;23/25/2016\n",$ligne[2]);
 	}
 
@@ -46,6 +46,11 @@ class TestTasks extends PHPUnit_Framework_TestCase{
 		$lignes = searchtask("Test");
 
 		$this->assertEquals("2;:!:;Test;:!:;23-01-2016;:!:;This is a test\n",$lignes);
+		
+		uptask("tache et plus");
+		$lignes = search("tache et plus");
+		
+		$this->assertEquals("2;:!:;tache et plus;:!:;23/25/2016\n",$lignes);
 
 		uptask("Test3",2);
 		$lignes = searchtask("Test3");
@@ -60,13 +65,19 @@ class TestTasks extends PHPUnit_Framework_TestCase{
 		$ligne = searchtask("Test3.1");
 
 		$this->assertEquals("1;:!:;Test3.1;:!:;24/26/2017;:!:;newcontent\n",$ligne);
+		
+		edittask(3,"tache et plus","24/26/2017","","tache et plus");
+		
+		$lignes = searchtask("tache et plus");
+		
+		$this->assertEquals("3;:!:;tache et plus;:!:;23/25/2016\n",$lignes);
 	}
 
 	public function testDeletetask()
 	{
-		deletetask("test 2");
+		deletetask("tache et plus");
 
-		$ligne = searchtask("test 2");
+		$ligne = searchtask("tache et plus");
 
 		$this->assertEquals(0,$ligne);
 	}
